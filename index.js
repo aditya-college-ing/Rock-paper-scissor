2e3b5ae2a91fa4906f1cb72be2811a621b095808
Lakhwinder <singhaditya1024@gmail.com>
2025-04-20 00:54:24 +0545

    Game-clone

---
+++ index.js
@@ -0,0 +1,60 @@
+ let userScore = 0;
+let computerScore = 0;
+
+let choices = document.querySelectorAll(".choice");
+let msg = document.querySelector("#msg"); 
+let userscorepara = document.querySelector("#your-score");
+let computerscorepara = document.querySelector("#computer-score");
+//user choice
+choices.forEach((choice) =>{
+    choice.addEventListener("click",() =>{
+        let userchoice = choice.getAttribute("id");
+        playgame(userchoice);
+    })
+})
+// winner status
+const showwinner = (userwin, userchoice, computer) =>{
+if(userwin){
+    userScore++;
+    userscorepara.innerText = userScore;
+    msg.innerText = `You win your ${userchoice} beats ${computer}`;
+    msg.style.backgroundColor = "lime";
+}else{
+    computerScore++
+    computerscorepara.innerText = computerScore;
+  msg.innerText = `You Lose ${computer} beats your ${userchoice}`;
+  msg.style.backgroundColor = "red";
+ };
+};
+//running status
+const playgame = (userchoice) =>{
+    console.log("user choice =",userchoice)
+    const computer = computerchoice()
+    console.log("computer choice =",computer)
+    if(userchoice === computer){
+        DrawGame();
+    }else{
+        let userwin = true;
+        if(userchoice === "rock"){
+           userwin = computer ==="paper"? false:true;
+        }else if(userchoice === "paper"){
+          userwin = computer === "scissor"? false:true;
+        }else{
+           userwin = computer === "rock"? false:true;
+        }
+        showwinner(userwin , userchoice, computer)
+    }
+}
+//computer choice
+const computerchoice = () =>{
+    const options = ["rock","paper","scissor"];
+    const randomidx = Math.floor(Math.random()*3);
+    return options[randomidx]
+}
+//Draw condition
+const DrawGame = () => {
+    console.log("Match Was Draw");
+    msg.innerText = "Match Draw";
+    msg.style.backgroundColor = "yellow"
+ }
+//END OF THIS CODE 
